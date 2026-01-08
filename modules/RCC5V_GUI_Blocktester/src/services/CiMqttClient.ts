35902d7839a7fb3090b4af8c25b938f3459c18ba
Karl Hartinger <karl@hartingers.at>
2026-01-08 17:10:59 +0100

    Update CiMqttClient

--- modules/RCC5V_GUI_Blocktester/src/services/CiMqttClient.ts
+++ modules/RCC5V_GUI_Blocktester/src/services/CiMqttClient.ts
@@ -72,7 +72,6 @@ export class CiMqttClient {
   public mqttState = readonly(this.privateMqttState)
 
   private privateMqttConnection: MqttConnection = reactive<MqttConnection>({
-    // host: '192.168.0.7',
     host: '10.1.1.1',
     port: 1884,
     endpoint: '',
