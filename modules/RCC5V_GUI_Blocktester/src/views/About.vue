867a10f010d693b1203a7315b0beba3a1297de45
Karl Hartinger <karl@hartingers.at>
2026-01-08 18:21:58 +0100

    Update views/About.vue

--- modules/RCC5V_GUI_Blocktester/src/views/About.vue
+++ modules/RCC5V_GUI_Blocktester/src/views/About.vue
@@ -1,17 +1,29 @@
 <!-- ----About.vue---------------------------khartinger----- -->
-<!-- 2025-12-27: New                                         -->
+<!-- 2026-01-08: New                                         -->
 <template>
   <div class="about">
-    <h2>RCC5V_GUI</h2>
-    Grafische Benutzeroberfl&auml;che zur Steuerung von RCC-Komponenten.<br>
+    <h2>RCC5V_GUI_Blocktester1</h2>
+    Grafische Benutzeroberfl&auml;che (GUI) zur Steuerung des Blocktesters für RCC-Komponenten.<br>
   </div>
   <div align="left">
+    <br>
+    <img src="../../images/o_GUI_Blocktester1.png" alt="GUI RCC-Blocktester"><br>
+    <i>Bild 1: GUI zur Steuerung des RCC-Blocktesters</i><br>   
+    <hr>
+    Der Bau und die Verwendung des Blocktesters f&uuml;r RCC-Komponenten ist in folgendem Repository beschrieben:<br>
+    <a href="https://github.com/khartinger/RCC5V/tree/main/examples/blocktester">https://github.com/khartinger/RCC5V/tree/main/examples/blocktester</a><br><br>
+    <img src="../../images/300_BT_view_front2.png" alt="Vorderansicht Blocktester"><br>
+    <i>Bild 2: Vorderansicht des Blocktesters</i><br>   
+
+    <img src="../../images/300_BT_view_above.png" alt="Draufsicht Blocktester"><br>
+    <i>Bild 3: Draufsicht auf den Blocktester</i><br>   
+
     <h3>Updates</h3>
-    2025-12-27 new<br/>
+    2026-01-08 New<br/>
     </div>
   <div class="about">
     <hr>
     Autor: DI Karl Hartinger<br>
-    Version: 2025-12-27<br>
+    Version: 2026-01-08<br>
   </div>
 </template>
