e0141abd94b8b2dc9488d28958e0924a5bba1dfc
Karl Hartinger <karl@hartingers.at>
2026-01-09 18:19:48 +0100

    Update RCC_GUI_Demo_Turnout3Way

--- examples/RCC5V_GUI_Demo_Turnout3Way/src/services/CiMqttClientInstance.ts
+++ examples/RCC5V_GUI_Demo_Turnout3Way/src/services/CiMqttClientInstance.ts
@@ -1,7 +1,7 @@
 // ______CiMqttClientInstance.ts_________________khartinger_____
-// 2025-12-27 New
+// 2026-01-09 New
 import { CiMqttClient } from './CiMqttClient'
-import { rccTo3way1Controller } from '@/controller/RccTurnout3Way1Controller'
+import { rccTurnout3Way1Controller } from '@/controller/RccTurnout3Way1Controller'
 
 export const ciMqttClientInstance = new CiMqttClient(true)
-ciMqttClientInstance.registerController(rccTo3way1Controller)
+ciMqttClientInstance.registerController(rccTurnout3Way1Controller)
--- examples/RCC5V_GUI_Demo_Turnout3Way/src/views/MainView.vue
+++ examples/RCC5V_GUI_Demo_Turnout3Way/src/views/MainView.vue
@@ -1,25 +1,42 @@
 <!-- ----MainView.vue------------------------khartinger----- -->
 <!-- 2026-01-06: New                                         -->
 <template>
-  <svg width="100%" viewBox="-45 -46 505 100">
-    <rect class="ciBackground" x="-45" y="-46" width="505" height="100" />
+  <svg width="100%" :viewBox="viewbox">
+    <rect class="ciBackground" :x="x1" :y="y1" :width="w1" :height="h1" />
     <text class="ciFont1" :x="-0.4*dx" :y="-0.55*dy">dir="1"</text>
     <text class="ciFont1" :x="0.8*dx" :y="-0.55*dy">dir="4"</text>
-    <RccTurnout3Way1 :x="0*dx"  :y="0*dy" sid="t3w1" dir="1" :border="1"></RccTurnout3Way1>
-    <RccTurnout3Way1 :x="1.2*dx"  :y="0*dy" sid="t3w1" dir="4" :border="1"></RccTurnout3Way1>
-    <RccTurnout3Way1 :x="2.8*dx"  :y="0*dy" sid="t3w1" dir="1" footer="UNTEN" :border="1"></RccTurnout3Way1>
-    <RccTurnout3Way1 :x="4*dx"  :y="0*dy" sid="t3w1" dir="4" header="        OBEN" :border="1"></RccTurnout3Way1>
+    <RccTurnout3Way1 :x="0*dx"  :y="0*dy" sid="t3w1" dir="1" :border="border"></RccTurnout3Way1>
+    <RccTurnout3Way1 :x="1.2*dx"  :y="0*dy" sid="t3w1" dir="4" :border="border"></RccTurnout3Way1>
+    <RccTurnout3Way1 :x="2.8*dx"  :y="0*dy" sid="t3w1" dir="1" footer="UNTEN" :border="border"></RccTurnout3Way1>
+    <RccTurnout3Way1 :x="4*dx"  :y="0*dy" sid="t3w1" dir="4" header="        OBEN" :border="border"></RccTurnout3Way1>
     <RccTurnout3Way1 :x="5.2*dx"  :y="0*dy" sid="t3w1" dir="4" label="3" :border="3"></RccTurnout3Way1>
   </svg>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import { Geo } from '../classes/Geo'
+import { computed, watchEffect } from 'vue'
+import { Geof } from '../classes/Geo'
+import { ciMqttClientInstance } from '@/services/CiMqttClientInstance'
+
 import RccTurnout3Way1 from '@/components/RccTurnout3Way1.vue'
+const border = 1
+
+// -----------waiting for MQTT connection, then get status------
+watchEffect(() => {
+  if (ciMqttClientInstance.mqttState.connected) {
+    ciMqttClientInstance.publish('rcc/demo1/get', 'status', false, 0).catch((e) => { console.error('Demo_Turnout3Way1: ERROR:', e) })
+  }
+})
 
 // ------Width and height of one Element (to make a grid)-------
-const geo = new Geo(0, 0)
-const dx = computed(() => geo.dxo())
-const dy = computed(() => geo.dyo())
+const geof = new Geof(0, 0, 1, 1)
+const dx = computed(() => geof.dxo()) // x width of a symbol in pixel (80)
+const dy = computed(() => geof.dyo()) // y heighth of a symbol in pixel (60)
+const nx = 6.5 //                        symbols in x direction
+const ny = 1 //                          symbols in x direction
+const x1 = computed(() => (-dx.value / 2 - 2)) // x coordinates top left corner
+const y1 = computed(() => (-dy.value - 2)) //     y coordinates top left corner
+const w1 = computed(() => (nx * dx.value + 4)) // viewbox width in pixel
+const h1 = computed(() => ((ny + 0.5) * dy.value + 4)) // viewbox height in pixel
+const viewbox = computed(() => [x1.value, y1.value, w1.value, h1.value].join(' ')) // svg viewbox
 </script>
