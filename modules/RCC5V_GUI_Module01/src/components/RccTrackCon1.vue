00704024ea4de3cb5857b99b26dc261dbb6190cb
Karl Hartinger <karl@hartingers.at>
2026-01-11 20:38:04 +0100

    Create RCC5V_GUI_Module01

---
+++ modules/RCC5V_GUI_Module01/src/components/RccTrackCon1.vue
@@ -0,0 +1,104 @@
+<!-- RccTrackCon1.vue -----------------------khartinger----- -->
+<!-- 2026-01-01: new                                         -->
+
+<template>
+  <g>
+  <path :d="drawCon1" :fill="colorTrackCon" :stroke="colorTrackCon" stroke-width="1" ></path>
+  <!-- ----------------------------------------------------- -->
+</g>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Geof } from '../classes/Geo'
+
+export default defineComponent({
+  name: 'RccTrackCon1',
+  components: {
+  },
+  data () {
+    return {
+      fw: 1 as number,
+    }
+  },
+  props: {
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
+    sid: {
+      type: String,
+      required: false,
+    },
+    dir: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: false,
+      default: '-',
+    },
+  },
+  computed: {
+    // =======standard methods==================================
+    // _______provide geometric data____________________________
+    geof: function (): Geof {
+      const geo1 = new Geof(this.x, this.y, 1, 1)
+      return geo1
+    },
+    drawCon1: function (): string {
+      return this.pathCon()
+    },
+    // _______color of the track________________________________
+    colorTrackCon: function (): string {
+      if (this.color !== '-') return this.color
+      /*
+      if (this.iTrack1State === 0) return this.geof.colorTrackOff
+      if (this.iTrack1State === 1) return this.geof.colorTrackOn
+      if (this.iTrack1State === 2) return this.geof.colorTrackUsed
+      if (this.iTrack1State === -99) return this.geof.colorTrackUnknown
+      */
+      return this.geof.colorTrack
+    },
+  },
+  methods: {
+    pathCon: function (): string {
+      // -----symbol "Section insulator" || --------------------
+      const dxo2 = this.geof.dxo2()
+      const dyo2 = this.geof.dyo2()
+      const tkcx = dxo2 - this.geof.tk.value[4].x
+      const tkcy = dyo2 + this.geof.tk.value[5].y
+      let s1 = ' M' + this.x + ',' + this.y
+      const dir_ = Number(this.dir)
+      switch (dir_) {
+        case 2: case 6:// === track left up /===================
+          // s1 += ' m' + (+dxo2) + ',' + (-dyo2)
+          s1 += ' v' + (-tkcy)
+          s1 += ' l' + (-tkcx) + ',' + (+tkcy)
+          s1 += ' z'
+          s1 += ' h' + (+tkcx)
+          s1 += ' l' + (-tkcx) + ',' + (+tkcy)
+          s1 += ' z'
+          break
+        case 4: case 8:// === track left down /=================
+          s1 += ' v' + (+tkcy)
+          s1 += ' l' + (-tkcx) + ',' + (-tkcy)
+          s1 += ' z'
+          s1 += ' h' + (+tkcx)
+          s1 += ' l' + (-tkcx) + ',' + (-tkcy)
+          s1 += ' z'
+          break
+        default:
+      } // ============= END: switch dir_ ======================
+      // console.log('pathIso: s1=', s1 + ' | dir_=' + dir_ + ' | part=' + part)
+      return s1
+    },
+  },
+})
+
+</script>
---
+++ modules/RCC5V_GUI_Module01/src/controller/RccUncoupler1Controller.ts
@@ -0,0 +1,64 @@
+// ______RccUncoupler1Controller.ts______________khartinger_____
+// 2026-01-08: new
+import { reactive } from 'vue'
+import { Message } from '@/services/CiMqttClient'
+import { CiBaseController, IBase } from './CiBaseController'
+
+export interface Uncoupler1 extends IBase {
+  iUncoupler1State: number;
+  sDCC: string,
+  textCenter?: string;
+  textFooter?: string;
+}
+
+export class RccUncoupler1Controller extends CiBaseController {
+  public payloadOn = '1'
+  public payloadOff = '0'
+  public sState0 = '0'
+  public sState1 = '1'
+  
+  public turnouts1: Array<Uncoupler1> = reactive(
+    [
+      {
+        // ---uncoupler platform 1------------------------------
+        id: 'uc1',
+        name: 'Uncoupler1',
+        iUncoupler1State: 0,
+        sDCC: '113',
+        subTopic: 'rcc/module01/ret/113 rcc/module01/ret/status',
+        pubTopic: 'rcc/module01/set/113',
+      },
+    ],
+  )
+
+  public onMessage (message: Message): void {
+    this.turnouts1.forEach(uncoupler1 => {
+      const aSubTopic = uncoupler1.subTopic.split(' ')
+      if (aSubTopic.includes(message.topic)) {
+        // ---uncoupler1 topic found -------------------------------
+        if (message.payload.length > 0) {
+          try {
+            const aPayload = JSON.parse(message.payload)
+            // const sDCC = String(uncoupler1.pubTopic.split('/').pop())
+            const sDCC_ = uncoupler1.sDCC
+            const sState_ = aPayload[sDCC_]
+            if (sState_ === this.sState0) uncoupler1.iUncoupler1State = 0
+            if (sState_ === this.sState1) uncoupler1.iUncoupler1State = 1
+          } catch (error) {
+            uncoupler1.iUncoupler1State = -99
+          }
+        }
+        // console.log('onMessage: topic=', message.topic + ', payload=' + message.payload)
+        // console.log('onMessage: uncoupler1.iUncoupler1State=', uncoupler1.iUncoupler1State)
+        // ---END: uncoupler1 topic found --------------------------
+      }
+    })
+  }
+
+  public publishCi (topic: string, payload: string): void {
+    // console.log('RccUncoupler1Controller:publishCi:', '-t ' + topic + ' -m ' + payload)
+    this.publish(topic, payload, false, 0).catch((e) => { console.error('RccUncoupler1Controller: ERROR:', e) })
+  }
+}
+
+export const rccUncoupler1Controller = new RccUncoupler1Controller()
