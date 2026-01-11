4b47bd70ff60798aaa2029028d06595f6d64b007
Karl Hartinger <karl@hartingers.at>
2026-01-11 20:37:07 +0100

    Create RCC5V_GUI_Module01

---
+++ modules/RCC5V_GUI_Module01/src/components/RccModule01.vue
@@ -0,0 +1,52 @@
+<!-- RccModule01.vue ------------------------khartinger----- -->
+<!-- 2026-01-11: new                                         -->
+<template>
+  <g>
+  <!-- empty fields ---------------------------------------- -->
+  <RccTrack1    :x="x0+0*dx" :y="y0+2*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <RccTrack1    :x="x0+1*dx" :y="y0+2*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <!-- row 1 ----------------------------------------------- -->
+  <RccTrack1     :x="x0+0*dx" :y="y0+0*dy" sid="tk1" dir="1" footer="11  [OFF]" footeralign="L" :border="border"></RccTrack1>
+  <RccTurnout1   :x="x0+1*dx" :y="y0+0*dy" sid="to12b" type="5L" header="12" headeralign="R" :border="border"></RccTurnout1>
+  <!-- row 2 ----------------------------------------------- -->
+  <RccTurnout1   :x="x0+0*dx" :y="y0+1*dy" sid="to12a" type="1L" header="12" headeralign="L" :border="border"></RccTurnout1>
+  <RccTrack1     :x="x0+1*dx" :y="y0+1*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
+  <!-- row 3 ----------------------------------------------- -->
+  <!-- rail connectors, isolators and module name----------- -->
+  <RccTrackIso1 :x="x0+0.5*dx" :y="y0+0.0*dy" sid="con0" dir="1"></RccTrackIso1>
+  <RccTrackCon1 :x="x0+0.5*dx" :y="y0+0.5*dy" sid="con0" dir="2"></RccTrackCon1>
+  <text class="ciFont1" :x="x0-0.48*dx" :y="y0-0.3*dy">Module 01</text>
+</g>
+</template>
+
+<script setup lang="ts">
+import { computed, watchEffect } from 'vue'
+import { Geo } from '../classes/Geo'
+import RccTrack1 from './RccTrack1.vue'
+import RccTrackCon1 from './RccTrackCon1.vue'
+import RccTrackIso1 from './RccTrackIso1.vue'
+import RccTurnout1 from './RccTurnout1.vue'
+import RccTurnout3Way1 from './RccTurnout3Way1.vue'
+import RccUncoupler1 from './RccUncoupler1.vue'
+import { ciMqttClientInstance } from '@/services/CiMqttClientInstance';
+
+const props = defineProps<{
+  x0: number;
+  y0: number;
+  sid: string;
+  border: number;
+}>()
+
+// -----------waiting for MQTT connection, then get status------
+watchEffect(() => {
+  if(ciMqttClientInstance.mqttState.connected) {
+    ciMqttClientInstance.publish('rcc/demo1/get', 'status', false, 0).catch((e) => { console.error('RccModule01: ERROR:', e) })
+  }
+})
+
+// ------Width and height of one Element (to make a grid)-------
+const geo = new Geo(0, 0)
+const dx = computed(() => geo.dxo())
+const dy = computed(() => geo.dyo())
+</script>
+
