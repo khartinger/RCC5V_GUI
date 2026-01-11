8de3775a3764e2621974fa064656936ce64bea02
Karl Hartinger <karl@hartingers.at>
2026-01-11 13:31:28 +0100

    New RCC5V_GUI_Module11

---
+++ modules/RCC5V_GUI_Module11/src/components/RccModule11.vue
@@ -0,0 +1,54 @@
+<!-- RccModule11.vue ------------------------khartinger----- -->
+<!-- 2026-01-11: new                                         -->
+<template>
+  <g>
+  <!-- empty fields ---------------------------------------- -->
+  <RccTrack1    :x="x0+2*dx" :y="y0+0*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <RccTrack1    :x="x0+0*dx" :y="y0+2*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <RccTrack1    :x="x0+1*dx" :y="y0+2*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <RccTrack1    :x="x0+2*dx" :y="y0+2*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <!-- row 1 ----------------------------------------------- -->
+  <RccUncoupler1 :x="x0+0*dx" :y="y0+0*dy" sid="uc1" dir="15" footer="113 [OFF]" footeralign="C" :border="border"></RccUncoupler1>
+  <RccTrack1     :x="x0+1*dx" :y="y0+0*dy" sid="tk1" dir="58" header="[ON]" headeralign="R" footer=" 112 [OFF]" footeralign="L" :border="border"></RccTrack1>
+  <!-- row 2 ----------------------------------------------- -->
+  <RccTrack1     :x="x0+0*dx" :y="y0+1*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
+  <RccTrack1     :x="x0+1*dx" :y="y0+1*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
+  <RccTurnout1   :x="x0+2*dx" :y="y0+1*dy" sid="to1" type="5R" header="111 " headeralign="R" :border="border"></RccTurnout1>
+  <!-- row 3 ----------------------------------------------- -->
+  <!-- rail connectors, isolators and module name----------- -->
+  <RccTrackIso1 :x="x0+1.5*dx" :y="y0+0.5*dy" sid="con0" dir="8"></RccTrackIso1>
+  <text class="ciFont1" :x="x0-0.48*dx" :y="y0-0.3*dy">Module 11</text>
+</g>
+</template>
+
+<script setup lang="ts">
+import { computed, watchEffect } from 'vue'
+import { Geo } from '../classes/Geo'
+import RccTrack1 from './RccTrack1.vue'
+//import RccTrackCon1 from './RccTrackCon1.vue'
+import RccTrackIso1 from './RccTrackIso1.vue'
+import RccTurnout1 from './RccTurnout1.vue'
+//import RccTurnout3Way1 from './RccTurnout3Way1.vue'
+import RccUncoupler1 from './RccUncoupler1.vue'
+import { ciMqttClientInstance } from '@/services/CiMqttClientInstance';
+
+const props = defineProps<{
+  x0: number;
+  y0: number;
+  sid: string;
+  border: number;
+}>()
+
+// -----------waiting for MQTT connection, then get status------
+watchEffect(() => {
+  if(ciMqttClientInstance.mqttState.connected) {
+    ciMqttClientInstance.publish('rcc/demo1/get', 'status', false, 0).catch((e) => { console.error('RccModule11: ERROR:', e) })
+  }
+})
+
+// ------Width and height of one Element (to make a grid)-------
+const geo = new Geo(0, 0)
+const dx = computed(() => geo.dxo())
+const dy = computed(() => geo.dyo())
+</script>
+
---
+++ modules/RCC5V_GUI_Module11/src/controller/RccTrack1Controller.ts
@@ -0,0 +1,71 @@
+// ______RccTrack1Controller.ts___________________khartinger_____
+// 2026-01-08: new
+import { reactive } from 'vue'
+import { Message } from '@/services/CiMqttClient'
+import { CiBaseController, IBase } from './CiBaseController'
+
+export interface Track1 extends IBase {
+  iTrack1State: number;
+  sDCC: string,
+  textCenter?: string;
+  textFooter?: string;
+  payloadInvert: boolean;
+}
+
+export class RccTrack1Controller extends CiBaseController {
+  public payloadTrackOn = '1'
+  public payloadTrackOff = '0'
+  public sState0 = '0'
+  public sState1 = '1'
+
+  public tracks1: Array<Track1> = reactive(
+    [
+      {
+        // ---track 2-------------------------------------------
+        id: 'tk1',
+        name: 'track1',
+        iTrack1State: -99,
+        sDCC: '112',
+        subTopic: 'rcc/module11/ret/112 rcc/module11/ret/status',
+        pubTopic: 'rcc/module11/set/112',
+        payloadInvert: false,
+      },
+    ],
+  )
+
+  public onMessage (message: Message): void {
+    this.tracks1.forEach(track1 => {
+      const aSubTopic = track1.subTopic.split(' ')
+      if (aSubTopic.includes(message.topic)) {
+      // ---track1 topic found -------------------------------
+        if (message.payload.length > 0) {
+          try {
+            const aPayload = JSON.parse(message.payload)
+            const sDCC_ = track1.sDCC
+            const sState_ = aPayload[sDCC_]
+            if (sState_ === this.sState0) {
+              track1.iTrack1State = 0
+              if (track1.payloadInvert) track1.iTrack1State = 1
+            }
+            if (sState_ === this.sState1) {
+              track1.iTrack1State = 1
+              if (track1.payloadInvert) track1.iTrack1State = 0
+            }
+            // console.log('onMessage: sState=', sState)
+          } catch (error) {
+            track1.iTrack1State = -99
+          }
+        }
+        // console.log('onMessage: track1.iTrack1State=', track1.iTrack1State)
+        // ---END: track1 topic found --------------------------
+      }
+    })
+  }
+
+  public publishCi (topic: string, payload: string): void {
+    // console.log('RccTrack1Controller:publishCi:', '-t ' + topic + ' -m ' + payload)
+    this.publish(topic, payload, false, 0).catch((e) => { console.error('RccTrack1Controller: ERROR:', e) })
+  }
+}
+
+export const rccTrack1Controller = new RccTrack1Controller()
