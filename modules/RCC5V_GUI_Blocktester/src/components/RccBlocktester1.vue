466f80c40acf3eed939be5b949b9dd91621ffce9
Karl Hartinger <karl@hartingers.at>
2026-01-12 18:34:58 +0100

    Update Blocktester

--- modules/RCC5V_GUI_Blocktester/src/components/RccBlocktester1.vue
+++ modules/RCC5V_GUI_Blocktester/src/components/RccBlocktester1.vue
@@ -1,11 +1,13 @@
 <!-- RccBlocktester1.vue --------------------khartinger----- -->
-<!-- 2026-01-08: new                                         -->
+<!-- 2026-01-12: new                                         -->
 <template>
   <g>
+  <!-- empty fields ---------------------------------------- -->
+  <RccTrack1    :x="x0+2*dx" :y="y0+0*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
+  <!-- row 1 ----------------------------------------------- -->
   <RccTrack1    :x="x0+0*dx" :y="y0+0*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
   <RccTrack1    :x="x0+1*dx" :y="y0+0*dy" sid="tk0" dir="58" :border="border"></RccTrack1>
-  <RccTrack1    :x="x0+2*dx" :y="y0+0*dy" sid="tk0" dir="0" :border="border"></RccTrack1>
-
+  <!-- row 2 ----------------------------------------------- -->
   <RccUncoupler1   :x="x0+0*dx" :y="y0+1*dy" sid="uc1" dir="15" header="[ON]" headeralign="C" footer="DCC 11" footeralign="C" :border="border"></RccUncoupler1>
   <RccTrack1       :x="x0+1*dx" :y="y0+1*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
   <RccTurnout3Way1 :x="x0+2*dx" :y="y0+1*dy" sid="t3w1" dir="4" header="DCC 32" headeralign="R" footer="DCC 31" footeralign="R" :border="border"></RccTurnout3Way1>
@@ -13,12 +15,22 @@
   <RccTrack1    :x="x0+0*dx" :y="y0+2*dy" sid="tk0" dir="15" :border="border"></RccTrack1>
   <RccTurnout1  :x="x0+1*dx" :y="y0+2*dy" sid="to1" type="1L" footer="DCC 21" :border="border"></RccTurnout1>  
   <RccTrack1    :x="x0+2*dx" :y="y0+2*dy" sid="tk1" dir="15" footer="DCC 41" footeralign="C" label="3" :border="border"></RccTrack1>
-
+  <!-- rail connectors, isolators, module name and border--- -->
   <RccTrackCon1 :x="x0+1.5*dx" :y="y0+0.5*dy" sid="con0" dir="8"></RccTrackCon1>
   <RccTrackCon1 :x="x0+1.5*dx" :y="y0+1.5*dy" sid="con0" dir="2"></RccTrackCon1>
   <RccTrackIso1 :x="x0+1.5*dx" :y="y0+2*dy" sid="iso0" dir="1"></RccTrackIso1>
+  <rect :x="x0-0.5*dx" :y="y0-0.5*dy" :width="bt1.nx*dx" :height="bt1.ny*dy" stroke="blue" stroke-width="4" fill="none" />
+  <text class="ciFont1" :x="x0-0.47*dx" :y="y0-0.27*dy">Blocktester</text>
+
 </g>
 </template>
+
+<script lang="ts">
+export const bt1 = {
+  nx: 3,
+  ny: 3
+}
+</script>
 
 <script setup lang="ts">
 import { computed, watchEffect } from 'vue'
--- modules/RCC5V_GUI_Blocktester/src/views/MainView.vue
+++ modules/RCC5V_GUI_Blocktester/src/views/MainView.vue
@@ -4,8 +4,6 @@
   <svg width="100%" :viewBox="viewbox">
     <rect class="ciBackground" :x="x1" :y="y1" :width="w1" :height="h1" />
     <RccBlocktester1 :x0="xB" :y0="yB" sid="bt1" :border="border" />
-    <!-- Frame around the blocktester ---------------------- -->
-    <rect :x="x1" :y="y1" :width="w1" :height="h1" stroke="blue" stroke-width="4" fill="none" />
   </svg>
 </template>
 
@@ -13,7 +11,7 @@
   import { computed, watchEffect } from 'vue'
   import { Geof } from '../classes/Geo'
   import { ciMqttClientInstance } from '@/services/CiMqttClientInstance'
-  import RccBlocktester1 from '@/components/RccBlocktester1.vue'
+  import RccBlocktester1, { bt1 } from '@/components/RccBlocktester1.vue'
   const border = 1
 
   // ---------waiting for MQTT connection, then get status------
@@ -29,8 +27,8 @@
   const geof = new Geof(0, 0, 1, 1)
   const dx = computed(() => geof.dxo()) // x width of a symbol in pixel (80)
   const dy = computed(() => geof.dyo()) // y heighth of a symbol in pixel (60)
-  const nx = 3            // 3 symbols in x direction
-  const ny = 3            // 3 symbols in x direction
+  const nx = bt1.nx // symbols in x direction
+  const ny = bt1.ny // 3 symbols in x direction
   const x1 = computed(() => (-dx.value/2 - 2))  // x coordinates top left corner
   const y1 = computed(() => (-dy.value/2 - 2))  // y coordinates top left corner
   const w1 = computed(() => (nx * dx.value + 4))  // viewbox width in pixel
